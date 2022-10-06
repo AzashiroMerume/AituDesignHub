7b6d8cdaa79bbbc821144dbda4f663d8f8aa2155
Winner13KS <kuandyksanzar020@gmail.com>
2022-10-06 14:29:26 +0600

    Created project, installed vue

---
+++ resources/js/components/footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="p-3 border-top">
+        <div class="container wrap">
+            <div class="row justify-content-center">
+                <div class="col-3"><h5>AituCourses</h5></div>
+                <div class="col-9">
+                    <a href="#" class="text-decoration-none mx-5 text-dark">Main</a>
+                    <a href="#" class="text-decoration-none mx-5 text-dark">Courses</a>
+                    <a href="#" class="text-decoration-none mx-5 text-dark">Podcasts</a>
+                    <a href="#" class="text-decoration-none mx-5 text-dark">Articles</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'footer-block',
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style>
+</style>
