e6c6673d475de49509eb756dc1c0f92e5c02014b
Winner13KS <kuandyksanzar020@gmail.com>
2022-10-08 21:15:56 +0600

    init

--- resources/js/views/main.vue
+++ resources/js/views/main.vue
@@ -1,28 +1,13 @@
-<template>
-    <header-block></header-block>
-    <best-popular-block></best-popular-block>
-    <hr class="mt-5" />
-    <courses-block title="Recomendations"></courses-block><br>
-    <footer-block class="mt-5"></footer-block>
+<template lang="">
+    <div>
+        <p>Hello man</p>
+    </div>
 </template>
 <script>
-import HeaderBlock from '../components/header.vue';
-import BestPopularBlock from '../components/bestpopular.vue';
-import CoursesBlock from '../components/courses.vue';
-import FooterBlock from '../components/footer.vue';
-
 export default {
-    name: "main",
-    data() {
-        return {
-
-        };
-    },
-    components: {
-        HeaderBlock,
-        BestPopularBlock,
-        CoursesBlock,
-        FooterBlock,
-    }
-};
+    name: 'main',
+}
 </script>
+<style lang="">
+    
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>How To Install Vue 3 in Laravel 9 with Vite</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>AituDesignHub</title>
 
     @vite('resources/css/app.css')
 </head>
